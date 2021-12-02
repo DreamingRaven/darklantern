d519ba6af6b0b17e93e7cb64492ba64037ed8f87
GeorgeRaven <GeorgeRavenCommunity@pm.me>
2021-12-02 10:37:47 +0000

    Added start to erray and its testing

---
+++ darklantern_test.go
@@ -0,0 +1,7 @@
+/**
+ * @Author: George Onoufriou <archer>
+ * @Date:   2021-12-02T08:28:56+00:00
+ * @Last modified by:   archer
+ * @Last modified time: 2021-12-02T08:29:37+00:00
+ */
+package darklantern
