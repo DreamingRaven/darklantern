e6c0f91a1b6b8ae400f229244f9e259c2776265d
GeorgeRaven <GeorgeRavenCommunity@pm.me>
2021-12-03 16:22:34 +0000

    Added lattigo to a test and to modfile

--- erray/erray_test.go
+++ erray/erray_test.go
@@ -2,14 +2,21 @@
  * @Author: George Onoufriou <archer>
  * @Date:   2021-12-02T10:02:50+00:00
  * @Last modified by:   archer
- * @Last modified time: 2021-12-02T10:37:14+00:00
+ * @Last modified time: 2021-12-02T21:16:37+00:00
  */
 package erray
 
 import (
-    "testing"
-  )
+	"fmt"
+	"testing"
 
-func TestErrayStruct (t *testing.T) {
-  t.Fatalf("Erray incomplete")
+	// "github.com/ldsec/lattigo/v2"
+	"github.com/ldsec/lattigo/v2/ckks"
+)
+
+func TestErrayStruct(t *testing.T) {
+	parms, _ := ckks.NewParametersFromLiteral(ckks.PN14QP438)
+	fmt.Printf("%T", parms)
+	t.Fatalf("Erray is incomplete")
+
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module gitlab.com/deepcypher/darklantern
 
 go 1.17
+
+require github.com/ldsec/lattigo/v2 v2.3.0
+
+require (
+	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
+	golang.org/x/sys v0.0.0-20211124211545-fe61309f8881 // indirect
+)
