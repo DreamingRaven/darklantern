aeb201f7eb966d0aa21012fb4d30723a290587a5
GeorgeRaven <GeorgeRavenCommunity@pm.me>
2021-12-07 22:27:02 +0000

    Added fledgling Erray struct and factory
    Begging by creating an example struct for CKKS type encryption called
    eCKKS that DOES NOT get exported from the package. Instead to aid in
    abstraction we enforce the use of the Erray interface from the
    NewCKKSErray factory. This means while we may now write functions with
    Erray + CKKS in mind we can simply add a new implementation of Erray
    using something else such as BFV or any further advancements in the
    future. This is only the beggining as Erray needs to be fleshed out with
    expected homomorphic function AS WELL AS being serialisable most likeley
    in JSON format for easy tansmission across networks and architectures.

--- erray/erray.go
+++ erray/erray.go
@@ -2,11 +2,11 @@
  * @Author: George Onoufriou <archer>
  * @Date:   2021-12-02T10:02:50+00:00
  * @Last modified by:   archer
- * @Last modified time: 2021-12-02T10:32:08+00:00
+ * @Last modified time: 2021-12-07T22:08:47+00:00
  */
 package erray
 
-type Erray struct {
+type eCKKS struct {
   shape []int
   data []float64
   state string
@@ -17,4 +17,10 @@ type Erray struct {
   encryptor string
   decryptor string
   evaluator string
+}
+
+type Erray interface {}
+
+func NewCKKSErray () Erray {
+  return eCKKS{}
 }
--- erray/erray_test.go
+++ erray/erray_test.go
@@ -2,7 +2,7 @@
  * @Author: George Onoufriou <archer>
  * @Date:   2021-12-02T10:02:50+00:00
  * @Last modified by:   archer
- * @Last modified time: 2021-12-02T21:16:37+00:00
+ * @Last modified time: 2021-12-07T22:24:38+00:00
  */
 package erray
 
@@ -11,12 +11,19 @@ import (
 	"testing"
 
 	// "github.com/ldsec/lattigo/v2"
-	"github.com/ldsec/lattigo/v2/ckks"
+	ckks "github.com/ldsec/lattigo/v2/ckks"
 )
 
 func TestErrayStruct(t *testing.T) {
 	parms, _ := ckks.NewParametersFromLiteral(ckks.PN14QP438)
 	fmt.Printf("%T", parms)
 	t.Fatalf("Erray is incomplete")
+}
+
+func TestNewCKKSErray(t *testing.T) {
+	var o Erray = NewCKKSErray()
+	oStr := fmt.Sprintf("%#v", o)
+	// oType := fmt.Sprintf("%T", o)
+	fmt.Printf(oStr)
 
 }
