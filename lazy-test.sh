a63cdc9305e5921502e93687e87038a3bc180c0b
GeorgeRaven <GeorgeRavenCommunity@pm.me>
2022-02-07 14:19:22 +0000

    Moved path control in lazy tester up

--- lazy-test.sh
+++ lazy-test.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 
-go env
-go mod tidy
-go test -v --cover ./...
-
 full_path=$(realpath $0)
 # echo $full_path
 dir_path=$(dirname $full_path)
 # echo $dir_path
+cd "${dir_path}"
+
+go env
+go mod tidy
+go test -v --cover ./...
+
 mkdir -p "${dir_path}/bin"
 
 GOOS=js GOARCH=wasm go build -o "${dir_path}/bin/test.wasm"
