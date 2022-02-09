474602e85284cc64d98d82a1ebea96a5482cd991
GeorgeRaven <GeorgeRavenCommunity@pm.me>
2022-02-09 15:57:52 +0000

    Added a number of get setters and sub automation
    Get and setters will now attempt to generate the values if they do not
    already exist as far as reasonably possible.

--- erray/eckks.go
+++ erray/eckks.go
@@ -15,13 +15,14 @@ type eCKKS struct {
 	data      *[]float64               // the message, plaintext, or cyphertext data
 	encrypted bool                     // whether 'cyphertext'=1 or 'plaintext'=0
 	params    *ckks.Parameters         // encoder/ encryptor parameters
-	sk        *rlwe.SecretKey          // generated secret key based on CKKS params
+	sk        *rlwe.SecretKey          // generated secret key based on CKKS params (SENSITIVE)
 	pk        *rlwe.PublicKey          // generated public key based on CKKS params
 	rlk       *rlwe.RelinearizationKey // generated relinearization key based on CKKS params
 	encoder   string                   // encoder instance to encode message to plaintext
 	encryptor string                   // encryptor instance of encoded polynomial
 	decryptor string                   // CKKS decryptor instance of cyphertext
 	evaluator string                   // CKKS computational circuit evaluator instance
+	// kgen      *rlwe.KeyGenerator       // generator for various keys (SENSITIVE)
 }
 
 // *****
@@ -49,7 +50,7 @@ func (eckks *eCKKS) SetData(newData *[]float64) error {
 	return nil
 }
 
-// GetData return message/ data from underlying eCKKS struct
+// Get existing data only
 func (eckks *eCKKS) GetData() *[]float64 {
 	return eckks.data
 }
@@ -59,7 +60,7 @@ func (eckks *eCKKS) SetShape(newShape []int) {
 	eckks.shape = newShape
 }
 
-// get imaginary shape of data
+// get existing imaginary shape of data only
 func (eckks *eCKKS) GetShape() []int {
 	return eckks.shape
 }
@@ -74,9 +75,94 @@ func (eckks *eCKKS) SetParams(newParams *ckks.Parameters) error {
 	return nil
 }
 
-// get encryption parameters
+// Get existing encryption parameters only
 func (eckks *eCKKS) GetParams() *ckks.Parameters {
 	return eckks.params
+}
+
+// Get existing secret key or attempt to generate a new one
+func (eckks *eCKKS) GetSK() (*rlwe.SecretKey, error) {
+	if eckks.sk == nil {
+		switch eckks.pk {
+		case nil:
+			err := eckks.InitKeys()
+			if err != nil {
+				return nil, err
+			}
+		default:
+			return nil, errors.New("SK does not exist, but other residual keys remain, aborting")
+		}
+	}
+	return eckks.sk, nil
+}
+
+// Get existing public key or attempt to generate a new one
+func (eckks *eCKKS) GetPK() (*rlwe.PublicKey, error) {
+	if eckks.pk == nil {
+		switch eckks.sk {
+		case nil:
+			err := eckks.InitKeys()
+			if err != nil {
+				return nil, err
+			}
+		default:
+			return nil, errors.New("eCKKS.GetPK() PK does not exist, but other residual keys remain, aborting")
+		}
+	}
+	return eckks.pk, nil
+}
+
+// Get existing relinearisation Key or attempt to generate a new one
+func (eckks *eCKKS) GetRK() (*rlwe.RelinearizationKey, error) {
+	if eckks.rlk == nil {
+		if eckks.pk == nil && eckks.sk == nil {
+			err := eckks.InitKeys()
+			if err != nil {
+				return nil, err
+			}
+		} else {
+			return nil, errors.New("eCKKS.GetRK() relin key does not exits, but there are other residual keys, aborting")
+		}
+	}
+	return eckks.rlk, nil
+}
+
+// Generate key set
+func (eckks *eCKKS) InitKeys() error {
+	fmt.Println("*************** I HAVE ATTEMPTED TO INIT KEYS ***********")
+	params := eckks.GetParams()
+	if params == nil {
+		return errors.New("eckks.InitKeys() has no encryption parameters to encrypt with")
+	}
+	// generate keys together
+	kgen := ckks.NewKeyGenerator(*params)
+	sk, pk := kgen.GenKeyPair()
+	rlk := kgen.GenRelinearizationKey(sk, 2)
+	// assign keys together
+	eckks.sk = sk
+	eckks.pk = pk
+	eckks.rlk = rlk
+	return nil
+}
+
+// GetEncoder if exists or attempt generation of new encoder
+func (eckks *eCKKS) GetEncoder() error {
+	return nil
+}
+
+// GetEncryptor if exists or attempt generation of new encryptor
+func (eckks *eCKKS) GetEncryptor() error {
+	return nil
+}
+
+// GetDecryptor if exists or attempt generation of new decryptor
+func (eckks *eCKKS) GetDecryptor() error {
+	return nil
+}
+
+// GetEvaluator if exists or attempt generation of new evaluator
+func (eckks *eCKKS) GetEvaluator() error {
+	return nil
 }
 
 // *********************
@@ -91,8 +177,10 @@ func (eckks *eCKKS) Encrypt() error {
 	if eckks.sk == nil && eckks.data != nil {
 		kgen := ckks.NewKeyGenerator(*eckks.params)
 		sk, pk := kgen.GenKeyPair()
+		rlk := kgen.GenRelinearizationKey(sk, 2)
 		fmt.Printf("Secret Key Type: %T\n", sk)
 		fmt.Printf("Public Key Type: %T\n", pk)
+		fmt.Printf("Relin  Key Type: %T\n", rlk)
 	}
 	return errors.New("Not yet implemented encryption.")
 }
