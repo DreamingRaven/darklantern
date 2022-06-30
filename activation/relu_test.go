7e2dbe51baaca60a4d62a16cf7dba063d0d84897
GeorgeRaven <GeorgeRavenCommunity@pm.me>
2022-06-30 13:54:46 +0100

    Removed old-style file headers

--- activation/relu_test.go
+++ activation/relu_test.go
@@ -1,9 +1,3 @@
-/**
- * @Author: George Onoufriou <archer>
- * @Date:   2021-12-02T09:47:59+00:00
- * @Last modified by:   archer
- * @Last modified time: 2021-12-02T11:09:28+00:00
- */
 package activation
 
 import (
