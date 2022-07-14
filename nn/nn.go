1bed91b29dbadcca575bea0e2592e3186e8556cd
GeorgeRaven <GeorgeRavenCommunity@pm.me>
2022-07-14 21:38:46 +0100

    Outlined NeuralNet abstraction

--- nn/nn.go
+++ nn/nn.go
@@ -1,1 +1,35 @@
 package nn
+
+import (
+	"fmt"
+	"sync"
+
+	"gitlab.com/deepcypher/darklantern/dager"
+)
+
+type neuralNet struct {
+	graph dager.Dager
+	// optimiser
+	// loss function
+	lock sync.RWMutex
+}
+
+type NeuralNet interface {
+	train() error
+	infer() error
+}
+
+// NewNeuralNet initialises a new neural network
+func NewNeuralNet() NeuralNet {
+	return &neuralNet{}
+}
+
+func (nn *neuralNet) train() error {
+	fmt.Println("Training")
+	return nil
+}
+
+func (nn *neuralNet) infer() error {
+	fmt.Println("Infering")
+	return nil
+}
--- nn/nn_test.go
+++ nn/nn_test.go
@@ -1,8 +1,11 @@
 package nn
 
 import (
+	"fmt"
 	"testing"
 )
 
 func TestNNInit(t *testing.T) {
+	nn := NewNeuralNet()
+	fmt.Println(nn)
 }
