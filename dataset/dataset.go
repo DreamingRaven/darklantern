958b2ee26707602b27d3b3cdf06119fadc56a0dd
GeorgeRaven <GeorgeRavenCommunity@pm.me>
2022-07-15 23:05:20 +0100

    Pushed generics through to working getter

--- dataset/dataset.go
+++ dataset/dataset.go
@@ -11,7 +11,7 @@ import (
 
 // Dataset abstraction and definition of avaliable methods
 type Dataset[D DatasetCompatible[L], L LattigoCompatible] interface {
-	Get(i int) error
+	Get(i int) (D, error)
 	Length() (int, error)
 	ToJSON() ([]byte, error)
 	FromJSON(bytes []byte) error
@@ -28,29 +28,28 @@ type DatasetCompatible[L LattigoCompatible] interface {
 }
 
 // exampleDataset the simplest dataset to show as an example
-type exampleDataset[L LattigoCompatible] struct {
-	data []L
+type exampleDataset[D DatasetCompatible[L], L LattigoCompatible] struct {
+	data []D
 }
 
 // NewExampleDataset initialises a new dataset object with some testable example data
 func NewExampleDataset[D DatasetCompatible[L], L LattigoCompatible]() Dataset[D, L] {
-	return &exampleDataset[L]{}
+	return &exampleDataset[D, L]{}
 }
 
 // Get a specific example from the dataset by index or error if impossible
-func (ds *exampleDataset[L]) Get(i int) error {
-	fmt.Println("Tr")
-	return nil
+func (ds *exampleDataset[D, L]) Get(i int) (D, error) {
+	return ds.data[i], nil
 }
 
 // Length of the dataset so controlling code does not exceed the bounds of this
-func (ds *exampleDataset[L]) Length() (int, error) {
+func (ds *exampleDataset[D, L]) Length() (int, error) {
 	fmt.Println("Tr")
 	return 0, nil
 }
 
 // ToJSON convert dataset internal struct to json bytes
-func (ds *exampleDataset[L]) ToJSON() ([]byte, error) {
+func (ds *exampleDataset[D, L]) ToJSON() ([]byte, error) {
 	marshalled, err := json.Marshal(ds)
 	if err != nil {
 		return nil, err
@@ -59,7 +58,7 @@ func (ds *exampleDataset[L]) ToJSON() ([]byte, error) {
 }
 
 // FromJSON convert json bytes back into original struct
-func (ds *exampleDataset[L]) FromJSON(bytes []byte) error {
+func (ds *exampleDataset[D, L]) FromJSON(bytes []byte) error {
 	err := json.Unmarshal(bytes, ds)
 	if err != nil {
 		return err
