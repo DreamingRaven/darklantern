f08ab9e26f590ee31142880b1147c77f69742bc3
GeorgeRaven <GeorgeRavenCommunity@pm.me>
2022-07-17 01:24:12 +0100

    Added sliceOfSlices test helper function

--- dataset/dataset_test.go
+++ dataset/dataset_test.go
@@ -7,17 +7,32 @@ import (
 	"gitlab.com/deepcypher/darklantern/erray"
 )
 
+// helper function to generate a dud example slice of slices
+func sliceOfSlices[T LattigoCompat](examples, features int) [][]T {
+	slice_slice := make([][]T, examples)
+	for i := 0; i < examples; i++ {
+		slice_slice[i] = make([]T, features)
+		for j := 0; j < features; j++ {
+			slice_slice[i][j] = T(i * j)
+		}
+	}
+	return slice_slice
+}
+
 func TestDatasetInit(t *testing.T) {
+	sliceOfSlices[float64](10, 20)
 	ds := NewSimpleDataset[erray.Erray[float64], float64]()
 	fmt.Println(ds)
 }
 
 func TestDatasetGet(t *testing.T) {
+	sliceOfSlices[float64](10, 20)
 	ds := NewSimpleDataset[erray.Erray[float64], float64]()
 	fmt.Println(ds)
 }
 
 func TestDatasetLen(t *testing.T) {
+	sliceOfSlices[float64](10, 20)
 	ds := NewSimpleDataset[erray.Erray[float64], float64]()
 	fmt.Println(ds)
 }
