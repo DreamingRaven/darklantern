67b58392eccba33a438d33daf01d615d8dcef32e
GeorgeRaven <GeorgeRavenCommunity@pm.me>
2022-07-18 00:19:24 +0100

    Split helpers into their own file

--- dataset/dataset_test.go
+++ dataset/dataset_test.go
@@ -4,21 +4,8 @@ import (
 	"fmt"
 	"testing"
 
-	"github.com/ldsec/lattigo/v2/ckks"
 	"gitlab.com/deepcypher/darklantern/erray"
 )
-
-// helper function to generate a dud example slice of slices
-func sliceOfSlices[T LattigoCompat](examples, features int) *[][]T {
-	slice_slice := make([][]T, examples)
-	for i := 0; i < examples; i++ {
-		slice_slice[i] = make([]T, features)
-		for j := 0; j < features; j++ {
-			slice_slice[i][j] = T(i * j)
-		}
-	}
-	return &slice_slice
-}
 
 func TestSliceOfSlices(t *testing.T) {
 	ss := sliceOfSlices[float64](10, 20)
@@ -29,23 +16,9 @@ func TestSliceOfSlices(t *testing.T) {
 	}
 }
 
-func sliceOfErrays[T LattigoCompat](sos *[][]T) *[]erray.Erray[T] {
-	soe := make([]erray.Erray[T], len(*sos))
-	e := erray.NewCKKSErray[T]()
-	parms, _ := ckks.NewParametersFromLiteral(ckks.PN12QP109)
-	e.SetParams(&parms)
-	for i := 0; i < len(*sos); i++ {
-		// soe[i] =
-		bud := e.Bud()
-		bud.SetData(&(*sos)[i])
-		soe[i] = bud
-	}
-	return &soe
-}
-
 func TestSliceOfErrays(t *testing.T) {
 	sos := sliceOfSlices[float64](10, 20)
-	soe := sliceOfErrays(sos)
+	soe := SliceOfErrays(sos)
 	for i := 0; i < len(*soe); i++ {
 		if (*soe)[i] == nil {
 			t.Fatal("Slice of Errays giving back empty errays pointers but should be populated")
@@ -56,14 +29,14 @@ func TestSliceOfErrays(t *testing.T) {
 
 func TestDatasetInit(t *testing.T) {
 	sos := sliceOfSlices[float64](10, 20)
-	soe := sliceOfErrays(sos)
+	soe := SliceOfErrays(sos)
 	NewSimpleDataset[erray.Erray[float64], float64](soe)
 }
 
 func TestDatasetGet(t *testing.T) {
 	// construct dataset from slice of slices
 	sos := sliceOfSlices[float64](10, 20)
-	soe := sliceOfErrays(sos)
+	soe := SliceOfErrays(sos)
 
 	ds := NewSimpleDataset[erray.Erray[float64], float64](soe)
 
@@ -87,7 +60,7 @@ func TestDatasetGet(t *testing.T) {
 
 func TestDatasetLen(t *testing.T) {
 	sos := sliceOfSlices[float64](10, 20)
-	soe := sliceOfErrays(sos)
+	soe := SliceOfErrays(sos)
 	ds := NewSimpleDataset[erray.Erray[float64], float64](soe)
 	dsLength, err := ds.Length()
 	if err != nil {
---
+++ dataset/helpers.go
@@ -0,0 +1,33 @@
+package dataset
+
+import (
+	"github.com/ldsec/lattigo/v2/ckks"
+	"gitlab.com/deepcypher/darklantern/erray"
+)
+
+// helper function to generate a dud example slice of slices
+func sliceOfSlices[T LattigoCompat](examples, features int) *[][]T {
+	slice_slice := make([][]T, examples)
+	for i := 0; i < examples; i++ {
+		slice_slice[i] = make([]T, features)
+		for j := 0; j < features; j++ {
+			slice_slice[i][j] = T(i * j)
+		}
+	}
+	return &slice_slice
+}
+
+// helper function to turn slice of slices into slice of errays
+func SliceOfErrays[T LattigoCompat](sos *[][]T) *[]erray.Erray[T] {
+	soe := make([]erray.Erray[T], len(*sos))
+	e := erray.NewCKKSErray[T]()
+	parms, _ := ckks.NewParametersFromLiteral(ckks.PN12QP109)
+	e.SetParams(&parms)
+	for i := 0; i < len(*sos); i++ {
+		// soe[i] =
+		bud := e.Bud()
+		bud.SetData(&(*sos)[i])
+		soe[i] = bud
+	}
+	return &soe
+}
