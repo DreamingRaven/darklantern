690aa6c3c243bc2fbaa8f1b33fe074d13617f105
GeorgeRaven <GeorgeRavenCommunity@pm.me>
2022-07-18 10:32:49 +0100

    Added util rais error

---
+++ utils/errors.go
@@ -0,0 +1,17 @@
+package utils
+
+import "log"
+
+func RaiseErrors(argv ...any) []any {
+	if argv[len(argv)-1] != nil {
+		log.Fatal(argv[len(argv)-1])
+	}
+	return argv[:len(argv)-1]
+}
+
+func RaiseError[T any](arg T, err error) T {
+	if err != nil {
+		log.Fatal(err)
+	}
+	return arg
+}
