3101248b7e50fe1d9cfc23d185d77c54e5e42989
GeorgeRaven <GeorgeRavenCommunity@pm.me>
2022-07-18 08:35:43 +0100

    Added simple dataloader func outline

--- dataloader/dataloader.go
+++ dataloader/dataloader.go
@@ -2,20 +2,31 @@ package dataloader
 
 import (
 	"fmt"
+
+	"gitlab.com/deepcypher/darklantern/dataset"
 )
 
-type dataloader struct {
-}
+// http://www.golangpatterns.info/concurrency/generators
+//  func generateRandomNumbers (n int) {
+//     ch := make (chan float)
+//     sem := make (semaphore, n)
+//     for i := 0; i < n; i++ {
+//         go func () {
+//             ch <- rand.Float()
+//             sem.Signal()
+//         } ()
+//     }
+//     // launch extra goroutine to eventually close ch
+//     go func () {
+//         sem.Wait(n)
+//         close(ch)
+//     }
+//     return ch
+// }
 
-type Dataloader interface {
-}
-
-// NewNeuralNet initialises a new neural network
-func NewExampleDataloader() Dataloader {
-	return &dataloader{}
-}
-
-func (ds *dataloader) train() error {
-	fmt.Println("Tr")
-	return nil
+// SimpleDataloader load data from dataset using goroutines concurrently based on number of workes, into batches, and whether or not to shuffle the dataset.
+func SimpleDataloader[D dataset.DatasetCompat[L], L dataset.LattigoCompat](ds dataset.Dataset[D, L], workers int, batchSize int, shuffle bool) {
+	fmt.Println("dataset", ds)
+	fmt.Println("workers", workers)
+	fmt.Println("batchSize", workers)
 }
--- dataloader/dataloader_test.go
+++ dataloader/dataloader_test.go
@@ -1,11 +1,15 @@
 package dataloader
 
 import (
-	"fmt"
 	"testing"
+
+	"gitlab.com/deepcypher/darklantern/dataset"
+	"gitlab.com/deepcypher/darklantern/erray"
 )
 
-func TestDataloaderInit(t *testing.T) {
-	ds := NewExampleDataloader()
-	fmt.Println(ds)
+func TestDataloading(t *testing.T) {
+	sos := dataset.ExampleSliceOfSlices[float64](10, 20)
+	soe := dataset.SliceOfErrays(sos)
+	ds := dataset.NewSimpleDataset[erray.Erray[float64], float64](soe)
+	SimpleDataloader(ds, 4, 32, true)
 }
